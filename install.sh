244f124a10d8b39ec202b23e0852db6d3d8cef1a
PoplarYang <echohiyang@foxmail.com>
2020-08-06 15:38:58 +0800

    add Makefile and update install.sh

--- install.sh
+++ install.sh
@@ -21,26 +21,74 @@
 #support
 
 base_url=http://qinius.echosoul.cn
+git_url=http://PoplarYang.github.io/files
 
 APPS=(
 bat
-qcmd
 bmon
+fd
+qcmd
+speedtest
+speedtest_go
+upto
 )
 
 bat=(
+bat-v0.15.4-x86_64-apple-darwin
+bat-v0.15.4-x86_64-unknown-linux-gnu
+bat-v0.15.4-arm-unknown-linux-gnueabihf
+bat-v0.15.4-aarch64-unknown-linux-gnu
+)
+
+bmon=(
+bmon.v4.0.darwin-amd64
+bmon.v3.6.linux-amd64
+bmon.v4.0.linux-mips64el
+)
+
+fd=(
+fd-v8.1.1-x86_64-apple-darwin
+fd-v8.1.1-x86_64-unknown-linux-gnu
+fd-v8.1.1-arm-unknown-linux-gnueabihf
+)
+
+qcmd=(
 qcmd.v0.4.2.darwin-amd64
-qcmd.v0.4.2.linux-386
 qcmd.v0.4.2.linux-amd64
 qcmd.v0.4.2.linux-arm64
 qcmd.v0.4.2.linux-mips64el
-qcmd.v0.4.2.windows-386.exe
 qcmd.v0.4.2.windows-amd64.exe
+)
+
+speedtest=(
+speedtest-mac-amd64-2.0.0-a-2-g18a889d
+speedtest-linux-amd64-2.0.0-a-2-g18a889d
+speedtest-linux-arm-2.0.0-a-2-g18a889d
+)
+
+speedtest_go=(
+speedtest-go.v1.0.3.darwin-amd64
+speedtest-go.v1.0.3.linux-amd64
+speedtest-go.v1.0.3.linux-arm64
+speedtest-go.v1.0.3.linux-mips64el
+speedtest-go.v1.0.3.windows-amd64.exe
+)
+
+upto=(
+upto.v0.0.1.darwin-amd64
+upto.v0.0.1.linux-amd64
+upto.v0.0.1.linux-arm64
+upto.v0.0.1.linux-mips64el
+upto.v0.0.1.windows-amd64.exe
+)
+
+FILES=(
+Makefile
 )
 
 function init() {
     echo -e "\tSelect operation type"
-    select app in INSTALL CONFIG REPO; do
+    select app in INSTALL CONFIG REPO FILES; do
         case $app in
 	    INSTALL)
 		install
@@ -50,6 +98,9 @@ function init() {
 		;;
 	    REPO)
 	        repo
+		;;
+	    FILES)
+		files
 		;;
 	    *)
                 echo "Wrong select, enter number."
@@ -72,6 +123,14 @@ function install() {
 #TODO: such as centos6/7/8, ubuntu16/18/20, debian9/10, Neokylin v7, Kylin v10,
 #function repo() {
 #}
+
+function files() {
+    echo -e "\tSelect wihch file to download"
+    select f in ${FILES[@]}; do
+        wget "$git_url/files/$f"
+	exit 0
+    done
+}
 
 function select_items() {
     item=$1
