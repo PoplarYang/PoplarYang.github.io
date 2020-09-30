443bf910af6ad6ae71e600218fb88e002caab920
PoplarYang <echohelloyang@gmail.com>
2020-09-30 10:58:34 +0800

    feat: repo support ubuntu

--- i.sh
+++ i.sh
@@ -2,11 +2,11 @@
 
 # *************************************************************
 # *                                                           *
-# * Name: install.sh                                          *
-# * Version: v0.0.5                                           *
+# * Name: i.sh                                                *
+# * Version: v0.0.6                                           *
 # * Function: quick install app base on arch and os           *
 # * Create Time: 2020-08-05                                   *
-# * Modify Time: 2020-09-29                                   *
+# * Modify Time: 2020-09-30                                   *
 # * Writen by PoplarYang (echohelloyang@gmail.com)            *
 # *                                                           *
 # *************************************************************
@@ -103,6 +103,7 @@ REPOS=(
 Centos-6
 Centos-7
 Centos-8
+ubuntu
 )
 
 function init() {
@@ -167,10 +168,36 @@ function config() {
 #TODO: such as centos6/7/8, ubuntu16/18/20, debian9/10, Neokylin v7, Kylin v10,
 function repo() {
     select f in ${REPOS[@]}; do
-        version=$(echo $f | awk -F '-' '{print $2}')
-        wget https://mirrors.aliyun.com/repo/Centos-${version}.repo
-        wget https://mirrors.aliyun.com/repo/epel-${version}.repo
-        exit 0
+        if [[ $f == Centos* ]]; then
+            version=$(echo $f | awk -F '-' '{print $2}')
+            wget https://mirrors.aliyun.com/repo/Centos-${version}.repo
+            wget https://mirrors.aliyun.com/repo/epel-${version}.repo
+            wget https://openresty.org/package/centos/openresty.repo
+            exit 0
+        elif [[ $f == "ubuntu" ]]; then
+            # add base repo
+            cat > /etc/apt/sources.list.d/base.list << EOF
+deb http://mirrors.aliyun.com/ubuntu/ $(lsb_release -sc) main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ $(lsb_release -sc)-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ $(lsb_release -sc)-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ $(lsb_release -sc)-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ $(lsb_release -sc)-backports main restricted universe multiverse
+EOF
+            # add ansible repo
+            add-apt-repository ppa:ansible/ansible
+
+            # add openresty repo
+            # 安装导入 GPG 公钥时所需的几个依赖包（整个安装过程完成后可以随时删除它们）：
+            sudo apt-get -y install --no-install-recommends wget gnupg ca-certificates
+
+            # 导入我们的 GPG 密钥：
+            wget -O - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
+
+            # 添加我们官方 APT 仓库：
+            echo "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main" \
+                | sudo tee /etc/apt/sources.list.d/openresty.list
+	    exit 0
+        fi
     done
 }
 
